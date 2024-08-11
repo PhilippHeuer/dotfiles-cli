3c033d1aa6e6f4bb122ba05bfdeb64874b8a7c32
Philipp Heuer <git@philippheuer.me>
2024-08-11 14:05:32 +0200

    feat: support env variables in theme commands

--- pkg/util/command.go
+++ pkg/util/command.go
@@ -7,6 +7,8 @@ import (
 
 // RunCommand executes a given shell command and returns an error if the command fails.
 func RunCommand(command string) error {
+	command = ResolvePath(command) // support placeholders such as ~ and $HOME
+
 	cmd := exec.Command("sh", "-c", command)
 
 	cmd.Stdout = os.Stderr
